fe1d43f3eace5b12922b299311175d841c68ac69
윤석규 <greeentea.dev@gmail.com>
2023-08-06 02:53:45 +0900

    Feat: Add application class

---
+++ app/src/main/java/kr/toongether/android/ToongetherApp.kt
@@ -0,0 +1,8 @@
+package kr.toongether.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToongetherApp : Application() {
+}
---
+++ app/src/main/java/kr/toongether/android/ui/ToongetherApp.kt
@@ -0,0 +1,9 @@
+package kr.toongether.android.ui
+
+import androidx.compose.runtime.Composable
+import kr.toongether.android.ToongetherApp
+
+@Composable
+fun ToongetherApp() {
+
+}
