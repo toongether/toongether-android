f602d6273065038ae655723d998bc8aa748a4675
윤석규 <greeentea.dev@gmail.com>
2023-08-06 16:58:33 +0900

    Feat: Add home navigation route
    add feature home module

---
+++ feature/home/src/main/java/kr/toongether/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package kr.toongether.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun HomeRoute() {
+    HomeScreen()
+}
+
+@Composable
+internal fun HomeScreen() {
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,4 @@ dependencyResolutionManagement {
 rootProject.name = "Toongether"
 include(":app")
 include(":core:designsystem")
+include(":feature:home")
