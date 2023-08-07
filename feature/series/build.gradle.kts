cca89aed1e065498e0d2a4e30ae3b9fcee37cab6
윤석규 <greeentea.dev@gmail.com>
2023-08-07 16:34:19 +0900

    Feat: Add pre release series ui

--- feature/series/build.gradle.kts
+++ feature/series/build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.material3)
+    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
--- feature/series/src/main/java/kr/toongether/series/SeriesScreen.kt
+++ feature/series/src/main/java/kr/toongether/series/SeriesScreen.kt
@@ -1,15 +1,131 @@
 package kr.toongether.series
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import kr.toongether.designsystem.theme.ToongetherTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kr.toongether.designsystem.component.ToongetherButton
+import kr.toongether.designsystem.component.ToongetherTopAppBar
+import kr.toongether.designsystem.icon.ToongetherIcons
+import kr.toongether.designsystem.icon.icons.Toongether
+import kr.toongether.designsystem.theme.pretendard
 
 @Composable
-internal fun SerialRoute() {
-    SerialScreen()
+internal fun SerialRoute(
+    modifier: Modifier = Modifier,
+    context: Context = LocalContext.current,
+) {
+    SerialScreen(
+        modifier = modifier,
+        context = context,
+    )
 }
 
 @Composable
-internal fun SerialScreen() {
-    ToongetherTheme {
+internal fun SerialScreen(
+    modifier: Modifier = Modifier,
+    context: Context,
+) {
+    Surface(
+        modifier = modifier
+            .fillMaxSize(),
+        color = Color.Black
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            ToongetherTopAppBar(
+                modifier = modifier
+                    .align(Alignment.TopCenter),
+                title = {
+                    Row(
+                        modifier = modifier
+                            .padding(bottom = 10.dp)
+                    ) {
+                        Box(
+                            modifier = modifier
+                                .fillMaxHeight()
+                        ) {
+                            Text(
+                                modifier = modifier.align(Alignment.BottomCenter),
+                                text = "연재 웹툰",
+                                fontFamily = pretendard,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White,
+                                fontSize = 26.sp
+                            )
+                        }
+                        Spacer(modifier = modifier.size(5.dp))
+                        Box(
+                            modifier = modifier
+                                .fillMaxHeight()
+                        ) {
+                            Text(
+                                modifier = modifier.align(Alignment.BottomCenter),
+                                text = "준비 중",
+                                fontFamily = pretendard,
+                                fontWeight = FontWeight.Normal,
+                                color = Color.White,
+                                fontSize = 14.sp
+                            )
+                        }
+                    }
+                }
+            )
+
+            Column(
+                modifier = modifier
+                    .align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "아직 사용할 수 없어요.",
+                    fontFamily = pretendard,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+
+                Spacer(modifier = modifier.size(10.dp))
+
+                ToongetherButton(
+                    onClick = {
+                        context.startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("https://toongether.kr/beta")
+                            )
+                        )
+                    }
+                ) {
+                    Text(
+                        text = "자세히 알아보기",
+                        fontFamily = pretendard,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp,
+                        color = Color.Black
+                    )
+                }
+            }
+        }
     }
 }
