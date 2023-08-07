9b761337c0cbdfbdfee8f8084dc0f1e57f7c209e
윤석규 <greeentea.dev@gmail.com>
2023-08-07 14:13:21 +0900

    Refactor: delete title text in navigation destination

--- app/src/main/java/kr/toongether/android/navigation/NavigationDestination.kt
+++ app/src/main/java/kr/toongether/android/navigation/NavigationDestination.kt
@@ -13,31 +13,25 @@ import kr.toongether.designsystem.icon.icons.Shorts
 enum class NavigationDestination(
     val icon: ImageVector,
     @StringRes val iconTextId: Int,
-    @StringRes val titleTextId: Int?,
 ) {
     HOME(
         icon = ToongetherIcons.Home,
         iconTextId = R.string.home,
-        titleTextId = null
     ),
     SERIES(
         icon = ToongetherIcons.Series,
         iconTextId = R.string.series,
-        titleTextId = R.string.title_series
     ),
     SHORTS(
         icon = ToongetherIcons.Shorts,
         iconTextId = R.string.shorts,
-        titleTextId = R.string.title_shorts
     ),
     COMMUNITY(
         icon = ToongetherIcons.Community,
         iconTextId = R.string.community,
-        titleTextId = R.string.community
     ),
     MY(
         icon = ToongetherIcons.My,
         iconTextId = R.string.my,
-        titleTextId = R.string.title_my
     )
 }
