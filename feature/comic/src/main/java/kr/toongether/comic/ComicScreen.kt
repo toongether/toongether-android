add56c4ca5bafac586ba8ebdb4e3d8ed03ab54d8
윤석규 <greeentea.dev@gmail.com>
2023-08-09 15:13:03 +0900

    Feat: Add tab animation

--- feature/comic/src/main/java/kr/toongether/comic/ComicScreen.kt
+++ feature/comic/src/main/java/kr/toongether/comic/ComicScreen.kt
@@ -1,32 +1,36 @@
 package kr.toongether.comic
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -35,8 +39,10 @@ import kr.toongether.designsystem.component.ToongetherTopAppBar
 import kr.toongether.designsystem.icon.ToongetherIcons
 import kr.toongether.designsystem.icon.icons.Back
 import kr.toongether.designsystem.theme.TransparentBlack
-import kr.toongether.designsystem.theme.pretendard
+import kr.toongether.designsystem.utils.NoRippleInteractionSource
 import org.orbitmvi.orbit.compose.collectAsState
+import java.time.LocalTime
+import kotlin.concurrent.timer
 
 @Composable
 internal fun ComicRoute(
@@ -48,26 +54,66 @@ internal fun ComicRoute(
     viewModel: ComicViewModel = hiltViewModel()
 ) {
     val comicState by viewModel.collectAsState()
-    viewModel.getComicList(id)
+    var isShowTabs by remember { mutableStateOf(true) }
+    var toggledTime: LocalTime? by remember { mutableStateOf(null) }
+
+    fun showTabs() {
+        if (toggledTime != null) {
+            toggledTime = null
+            isShowTabs = false
+        } else {
+            toggledTime = LocalTime.now().plusSeconds(3L)
+            isShowTabs = true
+            timer(period = 500) {
+                if (toggledTime == null) {
+                    isShowTabs = false
+                } else {
+                    if (LocalTime.now() > toggledTime) {
+                        isShowTabs = false
+                        toggledTime = null
+                    }
+                }
+            }
+        }
+    }
+
+    LaunchedEffect(true) {
+        viewModel.getComicList(id)
+        showTabs()
+    }
 
     ComicScreen(
         modifier = modifier,
         title = title,
         writer = writer,
-        onClickBack = { navController.popBackStack() },
-        comicState = comicState
+        onClickBack = navController::popBackStack,
+        comicState = comicState,
+        onClick = ::showTabs,
+        isShowTabs = isShowTabs
     )
 }
 
-@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 internal fun ComicScreen(
     modifier: Modifier = Modifier,
     title: String,
     writer: String,
     onClickBack: () -> Unit,
-    comicState: ComicState
+    comicState: ComicState,
+    onClick: () -> Unit,
+    isShowTabs: Boolean
 ) {
+    val lazyListState = rememberLazyListState()
+
+    val minHeight: Dp
+    val lastHeight: Dp
+
+    with(LocalDensity.current) {
+        val screenWidth = LocalConfiguration.current.screenWidthDp.dp.toPx()
+        minHeight = (screenWidth * comicState.comicList.height / comicState.comicList.height).toDp()
+        lastHeight = comicState.comicList.lastHeight.toDp()
+    }
+
     Surface(
         modifier = modifier
             .fillMaxSize(),
@@ -76,45 +122,86 @@ internal fun ComicScreen(
         Box(
             modifier = modifier
                 .fillMaxSize()
+                .clickable(
+                    interactionSource = NoRippleInteractionSource(),
+                    indication = null,
+                    onClick = onClick
+                )
         ) {
             LazyColumn(
-                modifier = modifier.fillMaxSize()
+                modifier = modifier
+                    .fillMaxSize(),
+                state = lazyListState
             ) {
                 itemsIndexed(
                     items = comicState.comicList.imageUrl,
                 ) { index, imageUrl ->
-                    GlideImage(
+                    if (index < comicState.comicList.endIndex)
+                        ComicItem(height = minHeight, imageUrl = imageUrl)
+                    else if (index == comicState.comicList.endIndex)
+                        ComicItem(height = lastHeight, imageUrl = imageUrl)
+
+                }
+            }
+            AnimatedVisibility(
+                visible = isShowTabs,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                ToongetherTopAppBar(
+                    modifier = modifier
+                        .height(90.dp)
+                        .background(TransparentBlack)
+                        .statusBarsPadding(),
+                    title = title,
+                    subTitle = writer,
+                    navigationIcon = ToongetherIcons.Back,
+                    onNavigationClick = onClickBack,
+                    backgroundColor = Color.Transparent
+                )
+            }
+            AnimatedVisibility(
+                modifier = modifier.align(Alignment.BottomCenter),
+                visible = isShowTabs,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                Surface(
+                    modifier = modifier
+                        .background(TransparentBlack)
+                        .navigationBarsPadding(),
+                    color = Color.Transparent
+                ) {
+                    Spacer(
                         modifier = modifier
-                            .fillMaxWidth(),
-                        model = imageUrl,
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop
+                            .fillMaxWidth()
+                            .height(35.dp)
                     )
                 }
             }
-
-            ToongetherTopAppBar(
-                modifier = modifier
-                    .background(TransparentBlack)
-                    .statusBarsPadding(),
-                title = title,
-                subTitle = writer,
-                navigationIcon = ToongetherIcons.Back,
-                onNavigationClick = onClickBack,
-                backgroundColor = Color.Transparent
-            )
-
-            Surface(
-                modifier = modifier
-                    .align(Alignment.BottomCenter)
-                    .background(TransparentBlack)
-                    .navigationBarsPadding(),
-                color = Color.Transparent
-            ) {
-                Spacer(modifier = modifier
-                    .fillMaxWidth()
-                    .height(35.dp))
-            }
         }
+    }
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+private fun ComicItem(
+    modifier: Modifier = Modifier,
+    height: Dp,
+    imageUrl: String,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = height)
+            .background(Color.Black)
+    ) {
+        GlideImage(
+            modifier = modifier
+                .fillMaxWidth(),
+            model = imageUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
     }
 }
