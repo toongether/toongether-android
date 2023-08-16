277167161ef545ef9d5bd9ae39cfef39ddf6ac5e
윤석규 <greeentea.dev@gmail.com>
2023-08-17 08:45:08 +0900

    Feat: Add signup module

---
+++ feature/auth/signup/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("toongether.android.library.compose")
+    id("toongether.android.feature")
+}
+
+android {
+    namespace = "kr.toongether.signup"
+}
+
+dependencies {
+    implementation(libs.androidx.compose.material.icons)
+}
