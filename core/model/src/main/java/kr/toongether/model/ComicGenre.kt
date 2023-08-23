6b721fc2246d5a619d0fb5770e33489fbd406c6b
윤석규 <greeentea.dev@gmail.com>
2023-08-24 00:49:51 +0900

    Refactor: Modify tab rows desgin

--- core/model/src/main/java/kr/toongether/model/ComicGenre.kt
+++ core/model/src/main/java/kr/toongether/model/ComicGenre.kt
@@ -1,8 +1,14 @@
 package kr.toongether.model
 
 enum class ComicGenre(val title: String) {
-    PRE_RELEASE("프리 릴리즈"),
-    EMPTY1(""),
-    EMPTY2(""),
-    EMPTY3("")
+        PRE_RELEASE("All"),
+    EMPTY1("월"),
+    EMPTY2("화"),
+    EMPTY3("수"),
+    EMPTY4("목"),
+    EMPTY5("금"),
+    EMPTY6("토"),
+    EMPTY7("일")
+//    EMPTY1("최근 본 웹툰"),
+//    EMPTY2("좋아하는 웹툰"),
 }
--- feature/my/src/main/java/kr/toongether/my/MyScreen.kt
+++ feature/my/src/main/java/kr/toongether/my/MyScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,9 +16,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import kotlinx.coroutines.launch
 import kr.toongether.designsystem.component.ToongetherButton
+import kr.toongether.designsystem.component.ToongetherScrollableTabRow
 import kr.toongether.designsystem.component.ToongetherTopAppBar
 import kr.toongether.designsystem.theme.pretendard
+import kr.toongether.model.ComicGenre
 import kr.toongether.signup.navigation.navigateToSignup
 
 @Composable
@@ -45,40 +49,18 @@ internal fun MyScreen(
             modifier = modifier
                 .fillMaxSize()
         ) {
-            ToongetherTopAppBar(
-                title = "마이 페이지",
-                subTitle = "준비 중"
-            )
-
-            Column(
-                modifier = modifier
-                    .align(Alignment.Center),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "아직 사용할 수 없어요.",
-                    fontFamily = pretendard,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 16.sp,
-                    color = Color.White
+            Column {
+                ToongetherTopAppBar(
+                    title = "마이 페이지",
                 )
 
-                Spacer(modifier = modifier.size(10.dp))
-
-                ToongetherButton(
-                    color = Color.White,
-                    onClick = {
-                        navController.navigateToSignup()
+                ToongetherScrollableTabRow(
+                    tabs = ComicGenre.values().toList().map { it.title },
+//                selectedTabIndex = pagerState.currentPage,
+                    selectedTabIndex = 0,
+                    onTabClick = { tabIndex ->
                     }
-                ) {
-                    Text(
-                        text = "로그인",
-                        fontFamily = pretendard,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 16.sp,
-                        color = Color.Black
-                    )
-                }
+                )
             }
         }
     }
