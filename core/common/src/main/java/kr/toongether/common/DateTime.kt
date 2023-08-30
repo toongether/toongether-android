d6aeea6636b0f325838ca793553c39322b905b7d
윤석규 <greeentea.dev@gmail.com>
2023-08-30 16:17:59 +0900

    Refactor: Modify calculate date logic

--- core/common/src/main/java/kr/toongether/common/DateTime.kt
+++ core/common/src/main/java/kr/toongether/common/DateTime.kt
@@ -3,14 +3,19 @@ package kr.toongether.common
 import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.todayIn
+import kotlinx.datetime.toLocalDateTime
+import kotlin.math.abs
 
 fun LocalDateTime.toRelativeDateTime(): String {
-    val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+    val today = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
     return if (this.year < today.year) "${today.year - this.year}년 전"
     else if (this.monthNumber < today.monthNumber) "${today.monthNumber - this.monthNumber}달 전"
     else if (this.dayOfMonth < today.dayOfMonth) "${today.dayOfMonth - this.dayOfMonth}일 전"
-    else "오래전"
+    else if (this.hour < today.hour) {
+        if (this.minute > today.minute) "${60 - abs(this.minute - today.minute)}분 전"
+        else "${today.hour - this.hour}시간 전"
+    }
+    else "방금 전"
 }
 
 fun LocalDateTime.toSimpleDate(): String =
