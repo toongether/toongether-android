b79cf867eb9d8e35b0879fa9d61ff3b34e9768c1
윤석규 <greeentea.dev@gmail.com>
2023-09-06 15:32:22 +0900

    Feat: Add series episode logic

---
+++ feature/episode/src/main/java/kr/toongether/episode/EpisodeViewModel.kt
@@ -0,0 +1,44 @@
+package kr.toongether.episode
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kr.toongether.domain.GetSeriesUseCase
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+@HiltViewModel
+class EpisodeViewModel @Inject constructor(
+    private val getSeriesUseCase: GetSeriesUseCase
+) : ContainerHost<EpisodeState, EpisodeSideEffect>, ViewModel() {
+
+    override val container = container<EpisodeState, EpisodeSideEffect>(EpisodeState())
+
+    fun getSeriesEpisode(id: Long) = intent {
+        reduce {
+            state.copy(
+                isLoading = true
+            )
+        }
+
+        getSeriesUseCase.invoke(id = id)
+            .onSuccess {
+                reduce {
+                    state.copy(
+                        seriesEpisode = it,
+                        isLoading = false
+                    )
+                }
+            }.onFailure {
+                postSideEffect(EpisodeSideEffect.Toast(it.message!!))
+                reduce {
+                    state.copy(
+                        isLoading = false
+                    )
+                }
+            }
+    }
+}
