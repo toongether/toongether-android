59932a367a3f564499598fa275d9b0497318a266
윤석규 <greeentea.dev@gmail.com>
2023-09-06 15:31:33 +0900

    Feat: Add episode screen

--- feature/episode/src/main/java/kr/toongether/episode/EpisodeScreen.kt
+++ feature/episode/src/main/java/kr/toongether/episode/EpisodeScreen.kt
@@ -1,22 +1,80 @@
 package kr.toongether.episode
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import kr.toongether.ui.TitleImageCard
+import kr.toongether.ui.seriesCardItems
+import org.orbitmvi.orbit.compose.collectAsState
 
 @Composable
 internal fun EpisodeRoute(
+    id: Long,
     modifier: Modifier = Modifier,
     navController: NavController,
+    viewModel: EpisodeViewModel = hiltViewModel()
 ) {
+    val state by viewModel.collectAsState()
 
+    LaunchedEffect(id) {
+        viewModel.getSeriesEpisode(id)
+    }
 
+    EpisodeScreen(
+        modifier = modifier,
+        episodeState = state
+    )
 }
 
 @Composable
 private fun EpisodeScreen(
     modifier: Modifier = Modifier,
-
+    episodeState: EpisodeState
 ) {
-    
+
+    Surface(
+        modifier = modifier
+            .fillMaxSize(),
+        color = Color.Black
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            TitleImageCard(
+                thumbnailImage = episodeState.seriesEpisode.titleInfo.thumbnailImage,
+                titleImage = episodeState.seriesEpisode.titleInfo.titleImage,
+                titleWidth = episodeState.seriesEpisode.titleInfo.titleWidth,
+                author = episodeState.seriesEpisode.author.name,
+                dayOfWeek = episodeState.seriesEpisode.dayOfWeek.title,
+                cycle = episodeState.seriesEpisode.cycle.title,
+                genre = episodeState.seriesEpisode.genre
+            )
+
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(vertical = 20.dp, horizontal = 12.dp)
+            ) {
+                seriesCardItems(
+                    items = episodeState.seriesEpisode,
+                    onItemClick = {
+
+                    }
+                )
+            }
+        }
+    }
 }
