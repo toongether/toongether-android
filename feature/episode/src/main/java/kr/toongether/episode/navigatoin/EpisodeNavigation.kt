607b2103b8a44c860a1f8afb4000ee0d8aef2188
윤석규 <greeentea.dev@gmail.com>
2023-09-08 10:06:36 +0900

    Refactor: Modify navigation animation

--- feature/episode/src/main/java/kr/toongether/episode/navigatoin/EpisodeNavigation.kt
+++ feature/episode/src/main/java/kr/toongether/episode/navigatoin/EpisodeNavigation.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavOptions
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 import com.google.accompanist.navigation.animation.composable
+import kr.toongether.comic.navigation.ComicRoute
 import kr.toongether.episode.EpisodeRoute
 
 const val EpisodeRoute = "episode_route/{id}"
@@ -23,10 +24,22 @@ fun NavGraphBuilder.episodeScreen(navController: NavController) {
         route = EpisodeRoute,
         arguments = listOf(navArgument("id") { type = NavType.LongType }),
         enterTransition = {
-            slideIntoContainer(AnimatedContentScope.SlideDirection.Left, animationSpec = tween(durationMillis = 400))
+            when (initialState.destination.route) {
+                "series_route" -> slideIntoContainer(
+                    AnimatedContentScope.SlideDirection.Left,
+                    animationSpec = tween(durationMillis = 400)
+                )
+                else -> null
+            }
         },
         exitTransition = {
-            slideOutOfContainer(AnimatedContentScope.SlideDirection.Right, animationSpec = tween(durationMillis = 400))
+            when (targetState.destination.route) {
+                "series_route" -> slideOutOfContainer(
+                    AnimatedContentScope.SlideDirection.Right,
+                    animationSpec = tween(durationMillis = 400)
+                )
+                else -> null
+            }
         }
     ) {
         EpisodeRoute(
